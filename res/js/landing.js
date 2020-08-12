0c3d4549214e36943e75ac248f5fb388b9dbdef8
Suritam-Nandy <13suritsuritam@gmail.com>
2020-08-12 10:51:41 +0530

    add animation to navbar

---
+++ res/js/landing.js
@@ -0,0 +1,25 @@
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+
+window.addEventListener(
+  "scroll",
+  (scrollFunction = () => {
+    if ($(window).width() >= 1024) {
+      document.body.scrollTop > 80 || document.documentElement.scrollTop > 80
+        ? smallBrandImg()
+        : largeBrandImg();
+    }
+  })
+);
+
+const smallBrandImg = () => {
+  document.getElementById("navbar-brand").style.padding = "8px";
+  document.getElementById("navbar-brand").style.width = "136px";
+  document.getElementById("navbar-brand").style.height = "80px";
+  document.getElementById("navbar-brand-img").style.width = "104px";
+};
+const largeBrandImg = () => {
+  document.getElementById("navbar-brand").style.padding = "16px";
+  document.getElementById("navbar-brand").style.width = "288px";
+  document.getElementById("navbar-brand").style.height = "138px";
+  document.getElementById("navbar-brand-img").style.width = "208px";
+};
