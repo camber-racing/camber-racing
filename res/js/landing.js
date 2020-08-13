680eb2e766b151058018b6adeabe3a82ea3f8d60
Suritam-Nandy <13suritsuritam@gmail.com>
2020-08-13 13:58:46 +0530

    add animation to brand image

--- res/js/landing.js
+++ res/js/landing.js
@@ -15,7 +15,10 @@ const smallBrandImg = () => {
   document.getElementById("navbar-brand").style.padding = "8px";
   document.getElementById("navbar-brand").style.width = "136px";
   document.getElementById("navbar-brand").style.height = "80px";
+  document.getElementById("navbar-brand").style.transitionDuration = ".5s";
+
   document.getElementById("navbar-brand-img").style.width = "104px";
+  document.getElementById("navbar-brand-img").style.transitionDuration = ".5s";
 };
 const largeBrandImg = () => {
   document.getElementById("navbar-brand").style.padding = "16px";
